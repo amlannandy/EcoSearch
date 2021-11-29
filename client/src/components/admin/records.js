550d9f0942071eb88a2750e8f35a49bf15abf47f
amlannandy <amlannandy5@gmail.com>
2021-11-29 21:12:16 +0530

    created admin ui

--- client/src/components/admin/records.js
+++ client/src/components/admin/records.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
+import { Card } from "antd-mobile";
 
+import "./css/records.css";
 import { getRecords } from "../../actions/index";
 
 function Records() {
@@ -15,9 +18,23 @@ function Records() {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Records</h1>
-    </div>
+    <>
+      {records.map(record => (
+        <Card key={record.id}>
+          <div className='card'>
+            <img src={record.imageUrl} alt='' className='recordImage' />
+            <div className='content'>
+              <h1>{record.title}</h1>
+              <p className='label'>{record.label}</p>
+            </div>
+            <div className='buttonsRow'>
+              <FaCheckCircle className='successIcon' size={35} />
+              <FaTimesCircle className='failureIcon' size={35} />
+            </div>
+          </div>
+        </Card>
+      ))}
+    </>
   );
 }
 
--- client/src/components/admin/researchers.js
+++ client/src/components/admin/researchers.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
+import { Card } from "antd-mobile";
 import { useSelector, useDispatch } from "react-redux";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 
+import "./css/researchers.css";
 import { getResearchers } from "../../actions/index";
 
 function Researchers() {
@@ -15,9 +18,25 @@ function Researchers() {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Researchers</h1>
-    </div>
+    <>
+      {researchers.map(user => (
+        <Card key={user.id}>
+          <div className='card'>
+            <img src={user.imageUrl} alt='' className='recordImage' />
+            <div className='col'>
+              <div className='content'>
+                <h1>{user.name}</h1>
+                <p>{user.email}</p>
+              </div>
+              <div className='buttonsRow'>
+                <FaCheckCircle className='successIcon' size={35} />
+                <FaTimesCircle className='failureIcon' size={35} />
+              </div>
+            </div>
+          </div>
+        </Card>
+      ))}
+    </>
   );
 }
 
